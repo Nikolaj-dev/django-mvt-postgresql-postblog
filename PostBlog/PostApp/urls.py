6e72037d8e6dcf1a0fb7e6ce76d3e50fb43a4a81
Nickolaj <nikolajpetrishchev@gmail.com>
2023-04-04 01:22:00 +0600

    add CRUD funcs for posts

--- PostBlog/PostApp/urls.py
+++ PostBlog/PostApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LogoutView
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.sign_up, name='sign_up'),
@@ -8,4 +10,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('posts/', views.all_posts, name='posts'),
     path('posts/<int:pk>/', views.detailed_posts, name='post'),
+    path('posts/create/', views.create_post, name='create_post'),
+    path('posts/update/<int:pk>/', views.update_post, name='update_post'),
+    path('posts/delete/<int:pk>/', views.delete_post, name='delete_post'),
 ]
--- PostBlog/PostApp/views.py
+++ PostBlog/PostApp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post
 
@@ -54,4 +54,43 @@ def detailed_posts(request: HttpRequest, pk: int) -> HttpResponse:
         "post": get_object_or_404(Post, pk=pk)
     }
     return render(request, 'post.html', context=context)
+
+
+def create_post(request: HttpRequest) -> HttpResponse:
+    context = {
+
+    }
+    if request.method == "POST":
+        title = request.POST['title']
+        body = request.POST['body']
+        Post.objects.create(
+            title=title,
+            body=body,
+        )
+        return redirect('posts')
+    return render(request, 'create_post.html', context=context)
+
+
+def update_post(request: HttpRequest, pk: int) -> HttpResponse:
+    post = Post.objects.filter(pk=pk)
+    context = {
+        "title_value": Post.objects.get(pk=pk).title,
+        "body_value": Post.objects.get(pk=pk).body
+    }
+    if request.method == "POST":
+        title = request.POST['title']
+        body = request.POST['body']
+        post.update(
+            title=title,
+            body=body,
+        )
+        return redirect('posts')
+    return render(request, 'update_post.html', context=context)
+
+
+def delete_post(request: HttpRequest, pk: int) -> HttpResponse:
+    post = Post.objects.filter(pk=pk)
+    post.delete()
+    return redirect("posts")
+
 
