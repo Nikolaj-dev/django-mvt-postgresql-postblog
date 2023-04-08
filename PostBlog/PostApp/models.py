6bb8b5b6087b111c3a24c42c4e8d7e5edceeb866
Nickolaj <nikolajpetrishchev@gmail.com>
2023-04-08 19:21:09 +0600

    add delete post image if post is being deleted

--- PostBlog/PostApp/models.py
+++ PostBlog/PostApp/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 
 class Post(models.Model):
@@ -11,6 +13,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+@receiver(pre_delete, sender=Post)
+def post_image_delete(sender, instance, **kwargs):
+    instance.image.delete(False)
 
 
 class PostLike(models.Model):
